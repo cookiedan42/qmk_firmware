ba01979d6c3693bbb22dcfca3e6809b262eae3f3
cookiedan42 <cookiedan42@gmail.com>
2024-01-24 16:14:17 +0800

    add my build

---
+++ keyboards/sofle/keymaps/cookiedan42/config.h
@@ -0,0 +1,7 @@
+#pragma once
+#define AUTO_SHIFT_MODIFIERS
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
